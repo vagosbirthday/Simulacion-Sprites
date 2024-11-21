755268e40b3c899275330320cb695627a1d4b6f7
Daniel Aguilar <daniel.aguilar7255@gmail.com>
2024-11-21 17:20:32 -0600

    first commit

---
+++ SpritesYSonido.py
@@ -0,0 +1,135 @@
+import pygame, sys, random
+from pygame.locals import *
+
+# configurar pygame
+pygame.init()
+relojPrincipal = pygame.time.Clock()
+
+# configurar la ventana
+ANCHOVENTANA = 400
+ALTOVENTANA = 400
+superficieVentana = pygame.display.set_mode((ANCHOVENTANA, ALTOVENTANA), 0, 32)
+pygame.display.set_caption('Sprites y Sonido')
+
+# configurar los colores
+NEGRO = (0, 0, 0)
+
+# cargar fondo
+imagenFondo = pygame.image.load('fondo.png')
+
+# configurar la estructura de bloque de datos
+jugador = pygame.Rect(300, 100, 40, 40)
+imagenJugador = pygame.image.load('jugador.png')
+imagenEstiradaJugador = pygame.transform.scale(imagenJugador, (40, 40))
+rotación = 0  # Ángulo inicial del jugador
+imagenComida = pygame.image.load('cereza.png')
+imagenComida = pygame.transform.scale(imagenComida, (20, 20))
+comidas = []
+for i in range(20):
+    comidas.append(pygame.Rect(random.randint(0, ANCHOVENTANA - 20), random.randint(0, ALTOVENTANA - 20), 20, 20))
+
+contadorComida = 0
+NUEVACOMIDA = 40
+
+# configurar variables del teclado
+moverseIzquierda = False
+moverseDerecha = False
+moverseArriba = False
+moverseAbajo = False
+
+VELOCIDADMOVIMIENTO = 6
+
+# configurar música
+sonidoRecolección = pygame.mixer.Sound('recolección.wav')
+pygame.mixer.music.load('músicaDeFondo.mid')
+pygame.mixer.music.play(-1, 0.0)
+músicaSonando = True
+
+# ejecutar el bucle del juego
+while True:
+    # comprobar si se ha disparado el evento QUIT (salir)
+    for evento in pygame.event.get():
+        if evento.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if evento.type == KEYDOWN:
+            # cambiar las variables del teclado
+            if evento.key == K_LEFT or evento.key == ord('a'):
+                moverseDerecha = False
+                moverseIzquierda = True
+            if evento.key == K_RIGHT or evento.key == ord('d'):
+                moverseIzquierda = False
+                moverseDerecha = True
+            if evento.key == K_UP or evento.key == ord('w'):
+                moverseAbajo = False
+                moverseArriba = True
+            if evento.key == K_DOWN or evento.key == ord('s'):
+                moverseArriba = False
+                moverseAbajo = True
+        if evento.type == KEYUP:
+            if evento.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+            if evento.key == K_LEFT or evento.key == ord('a'):
+                moverseIzquierda = False
+            if evento.key == K_RIGHT or evento.key == ord('d'):
+                moverseDerecha = False
+            if evento.key == K_UP or evento.key == ord('w'):
+                moverseArriba = False
+            if evento.key == K_DOWN or evento.key == ord('s'):
+                moverseAbajo = False
+            if evento.key == ord('x'):
+                jugador.top = random.randint(0, ALTOVENTANA - jugador.height)
+                jugador.left = random.randint(0, ANCHOVENTANA - jugador.width)
+            if evento.key == ord('m'):
+                if músicaSonando:
+                    pygame.mixer.music.stop()
+                else:
+                    pygame.mixer.music.play(-1, 0.0)
+                músicaSonando = not músicaSonando
+
+        if evento.type == MOUSEBUTTONUP:
+            comidas.append(pygame.Rect(evento.pos[0] - 10, evento.pos[1] - 10, 20, 20))
+
+    contadorComida += 1
+    if contadorComida >= NUEVACOMIDA:
+        # agregar nueva comida
+        contadorComida = 0
+        comidas.append(pygame.Rect(random.randint(0, ANCHOVENTANA - 20), random.randint(0, ALTOVENTANA - 20), 20, 20))
+
+    # pintar el fondo sobre la superficie
+    superficieVentana.blit(imagenFondo, (0, 0))
+
+    # mover el jugador
+    if moverseAbajo and jugador.bottom < ALTOVENTANA:
+        jugador.top += VELOCIDADMOVIMIENTO
+    if moverseArriba and jugador.top > 0:
+        jugador.top -= VELOCIDADMOVIMIENTO
+    if moverseIzquierda and jugador.left > 0:
+        jugador.left -= VELOCIDADMOVIMIENTO
+    if moverseDerecha and jugador.right < ANCHOVENTANA:
+        jugador.right += VELOCIDADMOVIMIENTO
+
+    # comprobar si el jugador ha intersectado alguno de los cuadrados de comida
+    for comida in comidas[:]:
+        if jugador.colliderect(comida):
+            comidas.remove(comida)
+            jugador = pygame.Rect(jugador.left, jugador.top, jugador.width + 2, jugador.height + 2)
+            imagenEstiradaJugador = pygame.transform.scale(imagenJugador, (jugador.width, jugador.height))
+            rotación -= 10  # Rotar 10 grados a la izquierda
+            if músicaSonando:
+                sonidoRecolección.play()
+
+    # dibujar al jugador con rotación
+    imagenRotadaJugador = pygame.transform.rotate(imagenEstiradaJugador, rotación)
+    jugadorCentro = (jugador.centerx - imagenRotadaJugador.get_width() // 2,
+                     jugador.centery - imagenRotadaJugador.get_height() // 2)
+    superficieVentana.blit(imagenRotadaJugador, jugadorCentro)
+
+    # dibujar la comida
+    for comida in comidas:
+        superficieVentana.blit(imagenComida, comida)
+
+    # dibujar la ventana sobre la pantalla
+    pygame.display.update()
+    relojPrincipal.tick(40)
